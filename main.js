0897c04c09550d7ee455c795de15d9abda0199b4
Arshia Shuli Chahellangi <90133461+Preak@users.noreply.github.com>
2021-12-04 22:10:53 +0330

    Add files via upload

---
+++ main.js
@@ -0,0 +1,108 @@
+function ale_error(text, Warning){
+  alert("error!, System & Connect to the Internet and try again.");
+  
+}
+function ale_Success(text, Warning){
+  alert("yes successfully completed you've done the right thing thanks.");
+}
+function ale_Warning(text, Warning){
+ alert("Do you want to do this? May damage your System");
+}
+function Console_error(text, value, Warning, console){
+  console.log("error!, System & Connect to the Internet and try again.");
+}
+function Console_Success(text, value, Warning, console){
+  console.log("yes successfully completed you've done the right thing thanks.");
+}
+function Console_Warning(text, value, Warning, console){
+  console.log("Do you want to do this? May damage your System");
+}
+function Chooser_nameTAG(){
+  document.getElementById("a");
+    document.getElementById("p");
+      document.getElementById("h1");
+        document.getElementById("h2");
+          document.getElementById("h3");
+  document.getElementById("h4");
+    document.getElementById("h5");
+      document.getElementById("h6");
+        document.getElementById("button");
+          document.getElementById("input");
+  document.getElementById("div");
+    document.getElementById("span");
+      document.getElementById("svg");
+        document.getElementById("fieldset");
+          document.getElementById("textarea");
+  document.getElementById("sub");
+    document.getElementById("sup");
+      document.getElementById("option");
+        document.getElementById("map");
+          document.getElementById("small");
+    document.getElementById("small");
+      document.getElementById("title");
+        document.getElementById("style");
+          document.getElementById("script");
+    document.getElementById("head");
+      document.getElementById("body");
+        document.getElementById("meta");
+          document.getElementById("link");
+            document.getElementById("title");
+              document.getElementById("from");
+                document.getElementById("finger");
+                  document.getElementById("ruby");
+                    document.getElementById("audio");
+                      document.getElementById("footer");
+                        document.getElementById("header");
+                          document.getElementById("selector");
+                            document.getElementById("select");
+                              document.getElementById("img");
+                                document.getElementById("label");
+                                  document.getElementById("datalist");
+  
+}
+function list_int(list){
+  console.log([0,1,2,3,4,5]);
+  }
+function $_PACH_list__int2(list){
+  console.log([0,1,2,3,4,5, 6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]);
+  }
+function $_PACH_list__int3(list){
+    console.log([0, 1, 2, 3, 4, 5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40]);
+  }
+  function $_PACH_list__int4(list){
+    console.log([0, 1, 2, 3, 4, 5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40, 41,42,43,44,45,46,47,48,49,50]);
+  }
+  function $__list_TEXT(list){
+    console.log([a, b, c, d, f, g]);
+  }
+  function $__list_TEXT_2(list){
+    console.log([ab, cd, fg]);
+  }
+ function operation(operation){
+   var x = 0;   var w = 10;    var q = 20;
+   var a = 1;   var t = 11;    var l = 21;
+   var b = 2;   var r = 12;    var k = 22;
+   var c = 3;   var o = 13;    var aa = 23;
+   var d = 4;   var i = 14;    var bb = 24;
+   var f = 5;   var u = 15;    var cc = 25;
+   var g = 6;   var v = 16;    var dd = 26;
+   var h = 7;   var z = 17;    var ff = 27;
+   var p = 8;   var s = 18;    var gg = 28;
+   var n = 9;   var m = 19;    var hh = 29;
+   
+   
+ }
+ exports.ale_error = ale_error;
+ exports.ale_Success = ale_Success;
+ exports.ale_Warning= ale_Warning;
+ exports.Console_error= Console_error;
+ exports.Console_Success= Console_Success;
+ exports.Console_Warning = Console_Warning;
+ exports.Chooser_nameTAG= Chooser_nameTAG;
+ exports.list_int= list_int;
+ exports.$_PACH_list__int2= $_PACH_list__int2;
+ exports.$_PACH_list__int3= $_PACH_list__int3;
+ exports.$_PACH_list__int4= $_PACH_list__int4;
+ exports.$__list_TEXT = $__list_TEXT;
+ exports.$__list_TEXT_2= $__list_TEXT_2;
+ exports.operation= operation;
